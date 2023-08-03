aae18098085e2644b1846bcde33eeb5ba7c98797
Juan Diego Bermudez Castañeda <juan052115@gmail.com>
2023-08-03 14:51:30 -0500

    Add the default option for filter notes

--- frontend/src/Components/CategoryList.jsx
+++ frontend/src/Components/CategoryList.jsx
@@ -5,6 +5,9 @@ const CategoryList = ({ categories }) => {
     <div className="Container">
       <h3>Category:</h3>
       <select>
+        <option value="All" key="All">
+          All
+        </option>
         {categories.map((category) => {
           return (
             <option value={category.title} key={category.id}>
